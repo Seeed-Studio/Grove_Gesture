acc93b567fd702b4162bfba8abfd60ac0be518cc
Jaxon <1391548050@qq.com>
2023-06-09 17:16:46 +0800

    Simplified code and initArray

--- paj7620.cpp
+++ paj7620.cpp
@@ -28,238 +28,28 @@
     THE SOFTWARE.
 */
 
-#include <Wire.h>
 #include "paj7620.h"
-#include <Arduino.h>
 
-// PAJ7620U2_20140305.asc
-/* Registers' initialization data */
 #if defined(__AVR__) || defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_ESP32)
 #define PROGMEM_COMPATIBLE
-const unsigned short initRegisterArray[] PROGMEM = {
+const uint8_t initRegisterArray[][2] PROGMEM = {
 #else
-const unsigned short initRegisterArray[] = {
+const uint8_t initRegisterArray[][2] = {
 #endif
-		// Initial Gesture // this is atleast 440 bytes
-		0xEF00,
-		0x3229,
-		0x3301,
-		0x3400,
-		0x3501,
-		0x3600,
-		0x3707,
-		0x3817,
-		0x3906,
-		0x3A12,
-		0x3F00,
-		0x4002,
-		0x41FF,
-		0x4201,
-		0x462D,
-		0x470F,
-		0x483C,
-		0x4900,
-		0x4A1E,
-		0x4B00,
-		0x4C20,
-		0x4D00,
-		0x4E1A,
-		0x4F14,
-		0x5000,
-		0x5110,
-		0x5200,
-		0x5C02,
-		0x5D00,
-		0x5E10,
-		0x5F3F,
-		0x6027,
-		0x6128,
-		0x6200,
-		0x6303,
-		0x64F7,
-		0x6503,
-		0x66D9,
-		0x6703,
-		0x6801,
-		0x69C8,
-		0x6A40,
-		0x6D04,
-		0x6E00,
-		0x6F00,
-		0x7080,
-		0x7100,
-		0x7200,
-		0x7300,
-		0x74F0,
-		0x7500,
-		0x8042,
-		0x8144,
-		0x8204,
-		0x8320,
-		0x8420,
-		0x8500,
-		0x8610,
-		0x8700,
-		0x8805,
-		0x8918,
-		0x8A10,
-		0x8B01,
-		0x8C37,
-		0x8D00,
-		0x8EF0,
-		0x8F81,
-		0x9006,
-		0x9106,
-		0x921E,
-		0x930D,
-		0x940A,
-		0x950A,
-		0x960C,
-		0x9705,
-		0x980A,
-		0x9941,
-		0x9A14,
-		0x9B0A,
-		0x9C3F,
-		0x9D33,
-		0x9EAE,
-		0x9FF9,
-		0xA048,
-		0xA113,
-		0xA210,
-		0xA308,
-		0xA430,
-		0xA519,
-		0xA610,
-		0xA708,
-		0xA824,
-		0xA904,
-		0xAA1E,
-		0xAB1E,
-		0xCC19,
-		0xCD0B,
-		0xCE13,
-		0xCF64,
-		0xD021,
-		0xD10F,
-		0xD288,
-		0xE001,
-		0xE104,
-		0xE241,
-		0xE3D6,
-		0xE400,
-		0xE50C,
-		0xE60A,
-		0xE700,
-		0xE800,
-		0xE900,
-		0xEE07,
-		0xEF01,
-		0x001E,
-		0x011E,
-		0x020F,
-		0x0310,
-		0x0402,
-		0x0500,
-		0x06B0,
-		0x0704,
-		0x080D,
-		0x090E,
-		0x0A9C,
-		0x0B04,
-		0x0C05,
-		0x0D0F,
-		0x0E02,
-		0x0F12,
-		0x1002,
-		0x1102,
-		0x1200,
-		0x1301,
-		0x1405,
-		0x1507,
-		0x1605,
-		0x1707,
-		0x1801,
-		0x1904,
-		0x1A05,
-		0x1B0C,
-		0x1C2A,
-		0x1D01,
-		0x1E00,
-		0x2100,
-		0x2200,
-		0x2300,
-		0x2501,
-		0x2600,
-		0x2739,
-		0x287F,
-		0x2908,
-		0x3003,
-		0x3100,
-		0x321A,
-		0x331A,
-		0x3407,
-		0x3507,
-		0x3601,
-		0x37FF,
-		0x3836,
-		0x3907,
-		0x3A00,
-		0x3EFF,
-		0x3F00,
-		0x4077,
-		0x4140,
-		0x4200,
-		0x4330,
-		0x44A0,
-		0x455C,
-		0x4600,
-		0x4700,
-		0x4858,
-		0x4A1E,
-		0x4B1E,
-		0x4C00,
-		0x4D00,
-		0x4EA0,
-		0x4F80,
-		0x5000,
-		0x5100,
-		0x5200,
-		0x5300,
-		0x5400,
-		0x5780,
-		0x5910,
-		0x5A08,
-		0x5B94,
-		0x5CE8,
-		0x5D08,
-		0x5E3D,
-		0x5F99,
-		0x6045,
-		0x6140,
-		0x632D,
-		0x6402,
-		0x6596,
-		0x6600,
-		0x6797,
-		0x6801,
-		0x69CD,
-		0x6A01,
-		0x6BB0,
-		0x6C04,
-		0x6D2C,
-		0x6E01,
-		0x6F32,
-		0x7100,
-		0x7201,
-		0x7335,
-		0x7400,
-		0x7533,
-		0x7631,
-		0x7701,
-		0x7C84,
-		0x7D03,
-		0x7E01};
+    {0xEF,0x00}, {0x37,0x07}, {0x38,0x17}, {0x39,0x06},
+    {0x42,0x01}, {0x46,0x2D}, {0x47,0x0F}, {0x48,0x3C},
+    {0x49,0x00}, {0x4A,0x1E}, {0x4C,0x20}, {0x51,0x10},
+    {0x5E,0x10}, {0x60,0x27}, {0x80,0x42}, {0x81,0x44},
+    {0x82,0x04}, {0x8B,0x01}, {0x90,0x06}, //{0x91,0x0A}
+    {0x95,0x0A}, {0x96,0x0C}, {0x97,0x05}, {0x9A,0x14},
+    {0x9C,0x3F}, {0xA5,0x19}, {0xCC,0x19}, {0xCD,0x0B},
+    {0xCE,0x13}, {0xCF,0x64}, {0xD0,0x21}, {0xEF,0x01},
+    {0x02,0x0F}, {0x03,0x10}, {0x04,0x02}, {0x25,0x01},
+    {0x27,0x39}, {0x28,0x7F}, {0x29,0x08}, {0x3E,0xFF},
+    {0x5E,0x3D}, {0x65,0x96}, {0x67,0x97}, {0x69,0xCD},
+    {0x6A,0x01}, {0x6D,0x2C}, {0x6E,0x01}, {0x72,0x01},
+    {0x73,0x35}, {0x77,0x01}, {0xEF,0x00},
+};
 
 /****************************************************************
     Function Name: paj7620WriteReg
@@ -335,64 +125,56 @@ uint8_t paj7620Init(void) {
     paj7620WriteReg(PAJ7620_REGITER_BANK_SEL, 0);
     paj7620WriteReg(PAJ7620_REGITER_BANK_SEL, 0);
 
-    error = paj7620ReadReg(0, 1, &data0);
-    if (error) {
-        return error;
-    }
-    error = paj7620ReadReg(1, 1, &data1);
-    if (error) {
-        return error;
-    }
-    Serial.print("Addr0 =");
-    Serial.print(data0, HEX);
-    Serial.print(",  Addr1 =");
-    Serial.println(data1, HEX);
-
+    paj7620ReadReg(0, 1, &data0);
+    paj7620ReadReg(1, 1, &data1);
     if ((data0 != 0x20) || (data1 != 0x76)) {
         return 0xff;
     }
     Serial.println("wake-up finish.");
 
-	for (i = 0; i < INIT_REG_ARRAY_SIZE; i++)
-	{
-        #ifdef PROGMEM_COMPATIBLE
-		uint16_t word = pgm_read_word(&initRegisterArray[i]);
-        #else
-        uint16_t word = initRegisterArray[i];
-        #endif
-		uint8_t high, low;
-		high = (word & 0xFF00) >> 8;
-		low = (word & 0x00FF);
-#ifdef debug
-		Serial.print("Line :");
-		Serial.print(i);
-		Serial.print(" data: 0x");
-		Serial.print(high, HEX);
-		Serial.println(low, HEX);
-#endif
-		paj7620WriteReg(high, low);
-    }
+	for(uint8_t i = 0; i < INIT_REG_ARRAY_SIZE; i++) {
+		#ifdef PROGMEM_COMPATIBLE
+		data0 = pgm_read_word(&initRegisterArray[i][0]);
+		data1 = pgm_read_word(&initRegisterArray[i][1]);
+		#else
+		data0 = initRegisterArray[i][0];
+		data1 = initRegisterArray[i][1];
+		#endif
+		paj7620WriteReg(data0, data1);
+	}
 
-    /**
-        Setting normal mode or gaming mode at BANK1 register 0x65/0x66 R_IDLE_TIME[15:0]
-        T = 256/System CLK = 32us,
-        Ex:
-        Far Mode: 1 report time = (77+R_IDLE_TIME)T
-        Report rate 120 fps:
-        R_IDLE_TIME=1/(120*T)-77=183
-        Report rate 240 fps:
-        R_IDLE_TIME=1/(240*T)-77=53
-        Near Mode: 1 report time = (112+R_IDLE_TIME)T
-        Report rate 120 fps:
-        R_IDLE_TIME=1/(120*T)-112=148
-        Report rate 240 fps:
-        R_IDLE_TIME=1/(240*T)-112=18
-    */
-    paj7620WriteReg(PAJ7620_REGITER_BANK_SEL, 1);  //gesture flage reg in Bank1
-    // paj7620WriteReg(0x65, 0xB7); // far mode 120 fps
-    paj7620WriteReg(0x65, 0x12);  // near mode 240 fps
+	setReportMode(NEAR_240FPS);
     paj7620WriteReg(PAJ7620_REGITER_BANK_SEL, 0);  //gesture flage reg in Bank0
 
     Serial.println("Paj7620 initialize register finished.");
     return 0;
+}
+
+/**
+    Setting normal mode or gaming mode at BANK1 register 0x65/0x66 R_IDLE_TIME[15:0]
+	T = 256/System CLK = 32us,
+*/
+void setReportMode(uint8_t reportMode) {
+	uint8_t regIdleTime = 0;
+	paj7620WriteReg(PAJ7620_REGITER_BANK_SEL, 1);  //gesture flage reg in Bank1
+	switch (reportMode) {
+	// Far Mode: 1 report time = (77 + R_IDLE_TIME) * T
+    case FAR_240FPS:
+		regIdleTime = 53; // 1/(240*T) - 77
+        break;
+    case FAR_120FPS:
+        regIdleTime = 183; // 1/(120*T) - 77
+        break;
+	// Near Mode: 1 report time = (112 + R_IDLE_TIME) * T
+    case NEAR_240FPS:
+        regIdleTime = 18; // 1/(240*T) - 112
+        break;
+	case NEAR_120FPS:
+        regIdleTime = 148; // 1/(120*T) - 112
+        break;
+    default:
+        break;
+    }
+	paj7620WriteReg(0x65, regIdleTime);
+	paj7620WriteReg(PAJ7620_REGITER_BANK_SEL, 0);  //gesture flage reg in Bank0
 }
